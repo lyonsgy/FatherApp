2d38da6c779c546c56e6c1f060f8ef49bed51bd5
gy_lyons <gy_lyons@163.com>
2019-02-25 10:32:20 +0800

    保存信息到 AppGroup.txt 文件中

--- FatherApp/ViewController.m
+++ FatherApp/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -16,8 +16,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textField.delegate = self;
+    self.textField.text = [CommonFuction getFatherMessage];
 }
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.textField resignFirstResponder];
+}
 
+//FatherApp 通过TextField来向共享文件appGroup.txt中写入数据
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    //获取App Group的共享目录
+    NSURL *groupURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.test.app.group.main"];
+    NSURL *fileURL = [groupURL URLByAppendingPathComponent:@"appGroup.txt"];
+    
+    //写入文件
+    [textField.text writeToURL:fileURL atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
 @end
